77795701b0a61e93598682a6af6ed06ee5dfbf63
Victor Fernandes <victor.hmfd@gmail.com>
2019-03-28 22:06:22 -0300

    Creating User Validator and Excepetion Handler
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- start/routes.js
+++ start/routes.js
@@ -16,5 +16,5 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.post('/users', 'UserController.store')
+Route.post('/users', 'UserController.store').validator('User')
 Route.post('/sessions', 'SessionController.store')
